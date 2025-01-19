ea7c750c9f387e2246a6c7e953ea0a348ddb67b1
Thomas <thomasrains01@gmail.com>
2025-01-18 20:02:30 -0800

    submits lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,16 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return (items[0])
+    elif len(items) == 2:
+        return (f"{items[0]} and {items[1]}")
+    elif len(items) == 3:
+        return (f"{items[0]}, {items[1]}, and {items[2]}")
+    elif len(items) >= 4:
+        oxford_list = []
+        for i in range(len(items)):
+            if i < len(items) -1:
+                oxford_list.append(f"{items[i]}, ")
+            elif i == len(items) -1:
+                oxford_list.append(f"and {items[i]}")
+        return "".join(oxford_list)
+
